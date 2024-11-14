6b180766390db32c352cb117a88ee61766ca6df5
Your Name <you@example.com>
2024-11-14 15:40:03 +0100

    Modified commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
+import Home from "./Pages/Home";
 import PublishArticle from "./Pages/Publish";
 import Journal from "./Pages/Journal";
-import Archive from "./pages/Archive";
+import Archive from "./Pages/Archive";
 import ProfessionalJournal from "./Pages/journals/Professional";
 import InnovationJournal from "./Pages/journals/Innovation";
 import NursingJournal from "./Pages/journals/Nursing";
